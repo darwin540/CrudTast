f515373756dc6a556497a500cdf833175cd1df84
darwin540 <steween22@gmail.com>
2024-11-29 10:13:54 -0500

    Creacion de proyecto

---
+++ src/test/java/org/novabitesolutions/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.novabitesolutions.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
